db796ce821aba0a35438a4dcface1e01a4af8de7
Owner <sagar.jate91@gmail.com>
2020-06-02 14:11:01 +0530

    update exception

--- src/main/java/com/secure/search/customer/exception/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/secure/search/customer/exception/GlobalDefaultExceptionHandler.java
@@ -8,27 +8,33 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 	
+	
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ModelAndView handlerNoHandlerFoundException()
 	{
+	
 		ModelAndView mv =new ModelAndView("error");
 		mv.addObject("errorTitle", "The Page is not Constructed!");
 		mv.addObject("errorDescription", "The Page you are looking for is not available now!");
 		mv.addObject("title", "404 Error Page");
 		return mv;
 	}
-	/*@ExceptionHandler(NullPointerException.class)
+	
+	@ExceptionHandler(NullPointerException.class)
 	public ModelAndView handlerNullPointerException()
 	{
+		
 		ModelAndView mv =new ModelAndView("error");
 		mv.addObject("errorTitle", "Null Pointer Exception!");
 		mv.addObject("errorDescription", "The Page you are looking for is not available now!");
 		mv.addObject("title", "404 Error Page");
 		return mv;
-	}*/
+	}
 
 	@ExceptionHandler(ProductNotFoundException.class)
 	public ModelAndView handlerProductNotFoundException() {
+		
+		
 		ModelAndView mv = new ModelAndView("error");
 		mv.addObject("errorTitle", "Product not available!");
 		mv.addObject("errorDescription", "The product you are looking for is not available right now!");
